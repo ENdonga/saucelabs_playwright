41bfb54208185e4fff149bf80a2b00e1da919745
ndonga <ndonga.edwin@gmail.com>
2024-10-04 09:52:47 +0300

    refactor login to use fixtures

---
+++ fixtures/login.fixture.ts
@@ -0,0 +1,28 @@
+import { test as baseTest } from "@playwright/test"
+import { LoginPage } from "../pages/login.page"
+import { HomePage } from "../pages/home.page";
+
+const USERNAME = "standard_user";
+const PASSWORD = "secret_sauce";
+
+type MyFixtures = {
+    login: () => Promise<void>;
+    homePage: HomePage;
+}
+
+export const test = baseTest.extend<MyFixtures>({
+    // fixture to provide login function
+    login: async ({ page }, use) => {
+        const loginPage = new LoginPage(page);
+        await loginPage.goto();
+        await loginPage.loginToApplication(USERNAME, PASSWORD);
+        await use(async ()=> {});
+    },
+
+    homePage: async({page}, use)=> {
+        const homePage = new HomePage(page)
+        await use(homePage)
+    }
+})
+
+export { expect } from '@playwright/test';
---
+++ tests/home.spec.ts
@@ -0,0 +1,12 @@
+import { test, expect } from '../fixtures/login.fixture'
+
+test.beforeEach(async ({ page, login }) => {
+  await login()
+})
+
+test.describe('Home page test', () => {
+  test('Add item to cart successfully', async ({homePage}) => {
+    await homePage.addItemToCart()
+    await expect(homePage.shoppingCartBadge).toBeVisible()
+  })
+})
